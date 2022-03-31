04350895dd8718b1619ff157313b50e9d9973c35
Ayato <45909629+ay4toh5i@users.noreply.github.com>
2022-03-31 10:47:27 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { ReactComponent as Logo } from './logo.svg';
+import './App.css';
+import { CountButton } from '@/components/CountButton';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Logo />
+        <p>Hello Vite + React!</p>
+        <p>
+          <CountButton />
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
